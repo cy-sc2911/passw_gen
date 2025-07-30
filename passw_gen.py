f48a506902300a4d3d25cee4f937583145c59d5d
cy-sc2911 <lalame1505@hotmail.com>
2025-07-30 13:31:32 +0800

    On branch main  Changes to be committed: 	modified:   passw_gen.py

--- passw_gen.py
+++ passw_gen.py
@@ -1,17 +1,27 @@
-import secrets
+import re        # use regular expressions/regex
+import secrets   # 
 import string
 
-def generate_password(length):
+def generate_password(length, nums, special_chars, uppercase, lowercase):
     alphabets = string.ascii_letters
     numbers = string.digits
     symbols = string.punctuation
 
     all_characters = alphabets + numbers + symbols
 
-    password = ""
-    # Generate password
-    for _ in range(length):
-        password += secrets.choice(all_characters)
+    while True:
+        password = ""
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+        constraints = [(nums, " ")]
+    
     return password
 
-print(generate_password(8))
+# The re.compile() function compiles the string passed as
+# the arugment into a regular expression object that
+# can be used by other 're' methods
+# the '+' quantifier means it should repeat one or more times
+pattern = re.compile('l+')
+quote = "Not all those who wander are lost."
+print(pattern.search(quote))
