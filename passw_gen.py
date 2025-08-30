6161c93d881b392188a356d9012c1b7f0cb5eb3c
cy-sc2911 <lalame1505@hotmail.com>
2025-08-30 13:19:09 +0800

    On branch main

--- passw_gen.py
+++ passw_gen.py
@@ -1,25 +1,34 @@
-import re        # use regular expressions/regex
-import secrets   # 
+import re
+import secrets
 import string
 
+
 def generate_password(length, nums, special_chars, uppercase, lowercase):
-    alphabets = string.ascii_letters
-    numbers = string.digits
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
     symbols = string.punctuation
 
-    all_characters = alphabets + numbers + symbols
+    # Combine all characters
+    all_characters = letters + digits + symbols
 
     while True:
-        password = ""
+        password = ''
         # Generate password
         for _ in range(length):
             password += secrets.choice(all_characters)
-        constraints = [(nums, r'\d'), # \d is a shorthand for [0-9]
-                       (lowercase, r'[a-z]'),
-                       (uppercase, r'[A-Z]'),
-                       (special_chars, fr'[{symbols}]') 
-                       # fr'[{symbols}]' safely builds a regex character class from dynamic input while preserving special characters
-                       ]
-                        # Check constraints
-    
+       
+        constraints = [
+            (nums, r'\d'),
+            (lowercase, r'[a-z]'),
+            (uppercase, r'[A-Z]'),            
+            (special_chars, fr'[{symbols}]')            
+        ]
+        # Check constraints
+        count = 0
+        for constraint, pattern in constraints:
+            constraint <= len(re.findall(pattern, password))
+
     return password
+# new_password = generate_password(8)
+# print(new_password)
